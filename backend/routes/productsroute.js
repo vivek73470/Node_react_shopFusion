e8c94ce881e79f19bb915cf04c1351a52e7cb089
vivek <vivek734706@gmail.com>
2024-09-24 23:38:34 +0530

    filter api

--- backend/routes/productsroute.js
+++ backend/routes/productsroute.js
@@ -2,6 +2,23 @@ const express = require('express')
 const AllProduct = require('../model/ProductModel/productmodel')
 
 const productsRoute = express.Router();
+
+// filter products
+productsRoute.get('/filter-products', async (req, res) => {
+    try {
+        const categories = req.query.category;
+
+        const filter = categories ? { category: { $in: Array.isArray(categories) ? categories : [categories] } } : {};
+
+        const products = await AllProduct.find(filter);
+
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+});
+
+
 // All products 
 productsRoute.get('/', async (req, res) => {
     try {
@@ -43,9 +60,9 @@ productsRoute.get('/:id', async (req, res) => {
 // add products
 productsRoute.post('/add', async (req, res) => {
     const { title, price, description, category, plp, brand_namez, discountedPriceText, actualPriceText,
-        discount_price_box, image} = req.body;
+        discount_price_box, image } = req.body;
     try {
-        const dataAdd = new AllProduct({title, price, description, category, plp, brand_namez, discountedPriceText, actualPriceText, discount_price_box, image});
+        const dataAdd = new AllProduct({ title, price, description, category, plp, brand_namez, discountedPriceText, actualPriceText, discount_price_box, image });
         await dataAdd.save()
         return res.status(200).send({
             status: true,
@@ -66,12 +83,12 @@ productsRoute.put('/:id', async (req, res) => {
     const payload = req.body;
     const id = req.params.id;
     try {
-       const data =  await AllProduct.findByIdAndUpdate({ _id: id }, payload);
-        return res.status(200).send({ 
-            status:true,
+        const data = await AllProduct.findByIdAndUpdate({ _id: id }, payload);
+        return res.status(200).send({
+            status: true,
             message: "update product successfully",
             data: data
-         });
+        });
     } catch (e) {
         console.log(e)
         return res.status(500).send({ message: "error while updating" });
@@ -80,19 +97,20 @@ productsRoute.put('/:id', async (req, res) => {
 })
 
 // delete product
-productsRoute.delete('/:id',async(req,res)=>{
+productsRoute.delete('/:id', async (req, res) => {
     const id = req.params.id;
-    try{
-     await AllProduct.findByIdAndDelete({_id: id});
-     return res.status(200).send({
-        status:true,
-        message: "deleted product"});
-    }catch(e){
+    try {
+        await AllProduct.findByIdAndDelete({ _id: id });
+        return res.status(200).send({
+            status: true,
+            message: "deleted product"
+        });
+    } catch (e) {
         console.log(e)
-        return res.status(500).send({message: "error while deleting"});
-    
+        return res.status(500).send({ message: "error while deleting" });
+
     }
-    })
+})
 
 
 module.exports = productsRoute;
